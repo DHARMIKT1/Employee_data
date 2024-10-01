150be1e1173e78c06baf8f7f0d87413f3c5ed1ed
Dharmik Tank <dharmikt70@gmail.com>
2024-10-01 22:09:55 +0530

    Add project files.

---
+++ Employee_data/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Employee_data.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Employee Registration</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 421px">
+            <h1>Employee Registration</h1>
+            <asp:TextBox ID="txtName" runat="server" placeholder="Enter Name"></asp:TextBox>
+            <br /><br />
+            <asp:TextBox ID="txtEmail" runat="server" placeholder="Enter Email"></asp:TextBox>
+            <br /><br />
+            <asp:TextBox ID="txtPhone" runat="server" placeholder="Enter Phone Number"></asp:TextBox>
+            <br /><br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+            &nbsp;<asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" Visible="false" />
+            &nbsp;<asp:Button ID="btnCancel" runat="server" Text="Cancel" OnClick="btnCancel_Click" Visible="false" />
+            <br /><br />
+            <asp:Label ID="lblMessage" runat="server" ForeColor="Red" />
+            <br /><br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowCommand="GridView1_RowCommand" DataKeyNames="Id">
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="Id" ReadOnly="True" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" />
+                    <asp:BoundField DataField="Phone" HeaderText="Phone" />
+                    <asp:ButtonField CommandName="EditRow" Text="Edit" ButtonType="Button" />
+                    <asp:ButtonField CommandName="DeleteRow" Text="Delete" ButtonType="Button" />
+                </Columns>
+            </asp:GridView>
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Employee_data/WebForm1.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Employee_data
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        private void BindGridView()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Employees", con))
+                {
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+        }
+
+        private void ClearFields()
+        {
+            txtName.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Employees (name, email, phone) VALUES (@name, @email, @phone)";
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@name", txtName.Text);
+                        cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                        cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+
+                        con.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            lblMessage.ForeColor = System.Drawing.Color.Green;
+                            lblMessage.Text = "Record Inserted";
+
+                            ScriptManager.RegisterStartupScript(this, GetType(), "HideLabel", "setTimeout(function(){ document.getElementById('" + lblMessage.ClientID + "').style.display='none'; }, 10000);", true);
+                        }
+                    }
+
+                }
+                BindGridView();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = (int)ViewState["EditId"];
+            string query = "UPDATE Employees SET name = @name, email = @email, phone = @phone WHERE id = @Id";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            BindGridView();
+            ClearFields();
+            btnUpdate.Visible = false;
+            btnSubmit.Visible = true;
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+            btnUpdate.Visible = false;
+            btnSubmit.Visible = true;
+            ViewState["EditId"] = null;
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "DeleteRow")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                int id = Convert.ToInt32(GridView1.DataKeys[index].Value);
+                DeleteRow(id);
+            }
+            else if (e.CommandName == "EditRow")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                int id = Convert.ToInt32(GridView1.DataKeys[index].Value);
+                EditRow(id);
+            }
+        }
+
+        private void DeleteRow(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Employees WHERE id = @Id";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            BindGridView();
+        }
+
+        private void EditRow(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Employees WHERE id = @Id";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    con.Open();
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    if (sdr.Read())
+                    {
+                        txtName.Text = sdr["Name"].ToString();
+                        txtEmail.Text = sdr["Email"].ToString();
+                        txtPhone.Text = sdr["Phone"].ToString();
+                        ViewState["EditId"] = id;
+
+                        btnUpdate.Visible = true;
+                        btnSubmit.Visible = false;
+                    }
+                }
+            }
+        }
+    }
+}
